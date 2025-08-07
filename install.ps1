f8588a68b214fb56b1e1995ca87a68e1b7ca5125
doggyisfunny <86542363+doggyisfunny@users.noreply.github.com>
2025-08-07 17:28:27 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,12 @@
-Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(''Hi HarrysTech!'',''Hello'')"'
+Add-Type -AssemblyName PresentationFramework
+$response = [System.Windows.MessageBox]::Show("Do you like HarrysTech?", "Important Question", "YesNo", "Question")
 
-exit
+if ($response -eq "Yes") {
+    Start-Process "https://www.tiktok.com/@harrystech_"
+    Start-Process "https://www.youtube.com/channel/UCM7SkpbDqI8n3MPXAoiNP-g"
+    Start-Process "https://www.instagram.com/harrystech_/"
+} else {
+    [System.Windows.MessageBox]::Show("Wrong answer. Shutting down in 10 seconds...", "Goodbye")
+    Start-Sleep -Seconds 10
+    Stop-Computer
+}
