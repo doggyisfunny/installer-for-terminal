0efc9e8a2da1b27861e46388a6afcc4c16e72543
doggyisfunny <86542363+doggyisfunny@users.noreply.github.com>
2025-08-07 17:20:36 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
-Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(''Hi from HarrysTech!'',''Hello'')"'
+Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(''Hi HarrysTech!'',''Hello'')"'
 
 exit
