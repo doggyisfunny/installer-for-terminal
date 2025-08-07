17f1995e71a5ce81f9a050a3deb102ae78c747fb
doggyisfunny <86542363+doggyisfunny@users.noreply.github.com>
2025-08-07 17:16:54 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,3 @@
-Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show('Hi from HarrysTech!', 'Hello')
+Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(''Hi from HarrysTech!'',''Hello'')"'
 
-Start-Sleep -Seconds 2
-
-# Close the PowerShell window
-$host.ui.RawUI.FlushInputBuffer()
 exit
