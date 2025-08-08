ceb1e69ebd08e40cf976e8ccb8455a0bb560db2a
doggyisfunny <86542363+doggyisfunny@users.noreply.github.com>
2025-08-08 10:05:11 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,12 +1,14 @@
-Add-Type -AssemblyName PresentationFramework
-$response = [System.Windows.MessageBox]::Show("Do you like HarrysTech?", "Important Question", "YesNo", "Question")
+$wallpaperUrl = "https://raw.githubusercontent.com/doggyisfunny/wallpapers/main/harrystech.png"
+$destination = "$env:USERPROFILE\Downloads\wallpaper.jpg"
 
-if ($response -eq "Yes") {
-    Start-Process "https://www.tiktok.com/@harrystech_"
-    Start-Process "https://www.youtube.com/channel/UCM7SkpbDqI8n3MPXAoiNP-g"
-    Start-Process "https://www.instagram.com/harrystech_/"
-} else {
-    [System.Windows.MessageBox]::Show("Wrong answer. Shutting down in 10 seconds...", "Goodbye")
-    Start-Sleep -Seconds 10
-    Stop-Computer
+Invoke-WebRequest -Uri $wallpaperUrl -OutFile $destination
+
+Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 }
+"@
+
+[Wallpaper]::SystemParametersInfo(20, 0, $destination, 3)
